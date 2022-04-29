a389900c8914b75e587adf61c72ac43dfd5dc154
msizza <nherizza.deguzman@outlook.com>
2022-04-29 06:13:39 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("afa2587a-c5a7-43ba-8d18-84c127b24faf");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
